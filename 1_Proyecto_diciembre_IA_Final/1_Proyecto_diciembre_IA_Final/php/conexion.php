15a3d58a0552c091f510ca1c9caa2adf865d7518
DanielCahisa <dcahisabenito@gmail.com>
2026-01-18 00:01:32 +0100

    Update conexion.php

--- 1_Proyecto_diciembre_IA_Final/1_Proyecto_diciembre_IA_Final/php/conexion.php
+++ 1_Proyecto_diciembre_IA_Final/1_Proyecto_diciembre_IA_Final/php/conexion.php
@@ -1,11 +1,18 @@
 <?php
-define('SERVIDOR','localhost');
-define('USUARIO','root');
-define('PASSWORD','');
-define('BASEDEDATOS','active360');
+// El servidor es el nombre del servicio en docker-compose
+define('SERVIDOR', 'mariadb');
 
-$con=new mysqli(SERVIDOR,USUARIO,PASSWORD,BASEDEDATOS);
+// El usuario que definiste en MYSQL_USER
+define('USUARIO', 'my_user');
+
+// La contraseña que definiste en MYSQL_PASSWORD
+define('PASSWORD', 'my_password');
+
+// La base de datos que definiste en MYSQL_DATABASE
+define('BASEDEDATOS', 'simple_attendance_db');
+
+$con = new mysqli(SERVIDOR, USUARIO, PASSWORD, BASEDEDATOS);
 if($con->connect_error){
-    die("Conexión fallida:". $con->connect_error);
+    die("Conexión fallida: " . $con->connect_error);
 }
 ?>
