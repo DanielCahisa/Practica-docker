225fe7215bf23f7295ec4d30cf251146ebd705b1
DanielCahisa <dcahisabenito@gmail.com>
2026-03-25 15:24:46 +0100

    Update conexion.php

--- 1_Proyecto_diciembre_IA_Final/1_Proyecto_diciembre_IA_Final/php/conexion.php
+++ 1_Proyecto_diciembre_IA_Final/1_Proyecto_diciembre_IA_Final/php/conexion.php
@@ -1,15 +1,8 @@
 <?php
-// El servidor es el nombre del servicio en docker-compose
 define('SERVIDOR', 'mariadb');
-
-// Usuario de la base de datos de la aplicación
 define('USUARIO', 'admin_asistencia');
-
-// Contraseña segura de la aplicación
 define('PASSWORD', 'SecurePass_Asist_2024!');
-
-// Nombre de la base de datos
-define('BASEDEDATOS', 'bd_control_asistencia');
+define('BASEDEDATOS', 'active360');
 
 $con = new mysqli(SERVIDOR, USUARIO, PASSWORD, BASEDEDATOS);
 if($con->connect_error){
